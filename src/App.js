31e96aff3a2ae1c390d7499348672020b51b9049
Corey Sawin <sawincp@gmail.com>
2023-06-12 15:01:48 -0400

    update App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+
 
 function App() {
+
+  const [movies, setMovies]= useState([])
+  const [reviews, setReviews]= useState([])
+  const [search, setSearch]= useState("")
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/movies")
+    .then((r)=> r.json)
+    .then((movies)=> setMovies(movies))
+  },[])
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/reviews")
+    .then((r)=> r.json)
+    .then((reviews)=> setReviews(reviews))
+  },[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
