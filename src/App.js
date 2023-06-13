51c5b092464bd0e0166232623445c58ef949ad03
Corey Sawin <sawincp@gmail.com>
2023-06-13 11:10:35 -0400

    import BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React, {useEffect, useState} from 'react'
+import Home from "./Home"
+import Movies from "./Movies"
 
 
 function App() {
 
-  const [movies, setMovies]= useState([])
-  const [reviews, setReviews]= useState([])
-  const [search, setSearch]= useState("")
+  // const [movies, setMovies]= useState([])
+  // const [reviews, setReviews]= useState([])
+  // const [search, setSearch]= useState("")
 
-  useEffect(()=>{
-    fetch("http://localhost:9292/movies")
-    .then((r)=> r.json)
-    .then((movies)=> setMovies(movies))
-  },[])
+  // useEffect(()=>{
+  //   fetch("http://localhost:9292/movies")
+  //   .then((r)=> r.json)
+  //   .then((movies)=> setMovies(movies))
+  // },[])
 
-  useEffect(()=>{
-    fetch("http://localhost:9292/reviews")
-    .then((r)=> r.json)
-    .then((reviews)=> setReviews(reviews))
-  },[])
+  // useEffect(()=>{
+  //   fetch("http://localhost:9292/reviews")
+  //   .then((r)=> r.json)
+  //   .then((reviews)=> setReviews(reviews))
+  // },[])
 
 
 
   return (
     <div className="App">
-    
+      <Home />
+      <Movies />
     </div>
   );
 }
