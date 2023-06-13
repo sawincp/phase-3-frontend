128b03968cb99a93b57a191166a50a03815f0084
Corey Sawin <sawincp@gmail.com>
2023-06-13 11:47:03 -0400

    updated Home component

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,13 @@ import React from 'react'
 
  const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Welcome to Cinema Critique!</h1>
+      <hr></hr>
+      <p>
+      Discover the ultimate destination for movie enthusiasts, where captivating stories unfold on the silver screen. At Cinema Critique, we strive to provide you with the most insightful and comprehensive movie reviews, keeping you informed and entertained. Whether you're a casual moviegoer or a dedicated cinephile, we have everything you need to make informed choices about the films you watch.
+      </p>
+    </div>
   )
 }
 
