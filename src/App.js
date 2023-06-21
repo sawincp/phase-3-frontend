3533f2f1818a3bdc162b3834a25fa53761bf9e20
Corey Sawin <sawincp@gmail.com>
2023-06-21 15:03:51 -0400

    update NavBar sytle & App component:

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React, {useEffect, useState} from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from "./Home"
-import Movies from "./Movies"
+import MovieList from "./MovieList"
 import NavBar from './NavBar'
 
 
 function App() {
 
-  // const [movies, setMovies]= useState([])
-  // const [reviews, setReviews]= useState([])
-  // const [search, setSearch]= useState("")
+  const [movies, setMovies]= useState([])
+  const [search, setSearch]= useState("")
 
-  // useEffect(()=>{
-  //   fetch("http://localhost:9292/movies")
-  //   .then((r)=> r.json)
-  //   .then((movies)=> setMovies(movies))
-  // },[])
+  useEffect(()=>{
+    fetch("http://localhost:9292/movies")
+    .then((r)=> r.json())
+    .then((movies)=> setMovies(movies))
+    // .then((movies)=> console.log(movies[0].reviews))
+  },[])
 
-  // useEffect(()=>{
-  //   fetch("http://localhost:9292/reviews")
-  //   .then((r)=> r.json)
-  //   .then((reviews)=> setReviews(reviews))
-  // },[])
+  if(!movies) return ("Loading...")
 
+  const displayMovies = movies.filter((movie)=>{
+    return movie.title.toLowerCase().includes(search.toLowerCase())
 
+  })
+
+  // const displayHeros = heros.filter((hero)=>{
+  //   return hero.alias.toLowerCase().includes(searchTerm.toLowerCase())
+  // })
 
   return (
     <div className="App">
       <NavBar/>
       <Routes>
         <Route exact path ="/" element={<Home />}/>
-        <Route exact path ="/movies" element= {<Movies />}/>
+        <Route exact path ="/movies" element= {<MovieList movies ={displayMovies} search ={search} onSearchChange = {setSearch}  />}/>
       </Routes>
     </div>
   );
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,10 +2,26 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
+
+  const linkStyles = {
+    display: "inline",
+    justifyContent: "center", // This centers items horizontally
+    alignItems: "center", // This centers items vertically
+    width: "50px",
+    height: "50px", // Added height to better visualize the effects of the justifyContent and alignItems properties 
+    padding: "20px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "underline",
+    color: "white",
+    fontWeight: "bold"
+  };
+
+  
   return (
     <div>
-        <NavLink to= "/" activeClassName= "active" style={{ fontWeight: 'bold' }}> Home | </NavLink>
-        <NavLink to= "/movies" activeClassName= "active" style={{ fontWeight: 'bold' }}> Movie List </NavLink>
+        <NavLink to= "/" activeClassName= "active" style={ linkStyles }> Home </NavLink>
+        <NavLink to= "/movies" activeClassName= "active" style={ linkStyles }> Movie List </NavLink>
     </div>
   )
 }
