4ec44880292bef867a7e064c6c4a78bbea0011e6
Corey Sawin <sawincp@gmail.com>
2023-06-27 15:33:49 -0400

    update Movie & MovieList. Remove EditGenre

--- src/Movie.js
+++ src/Movie.js
@@ -1,48 +1,14 @@
-import React from 'react'
-
-// import EditGenre from './EditGenre'
+import React from "react";
 
 
-const Movie = ({id, title, genre, onUpdatedGenre}) => {
+const Movie = ({ title, genre, }) => {
 
-  // const [isEditing, setIsEditing]= useState(false)
-
-
-  // function handleUpdatedGenre (updateGerne){
-  //   setIsEditing(false)
-  //   onUpdatedGenre(updateGerne)
-  // }
 
   return (
     <div>
-      <p>{id}</p>
       <p>{title}</p>
-      <p>{genre}</p>
+      <p>{genre}</p> 
     </div>
-
-
-
-    // <Container>
-    //   <Row>
-    //     <Col><p>{id}</p></Col>
-    //     <Col><p>{title}</p></Col>
-    //     <Col>
-    //         <button onClick={() => setIsEditing((isEditing) => !isEditing)}>
-    //           <span role="img" aria-label="edit">
-    //             ✏️
-    //           </span>
-    //         </button>
-    //       {isEditing ? (
-    //         <EditGenre
-    //           id={id}
-    //           genre={genre}
-    //           onUpdateGenre={handleUpdatedGenre}
-    //         />
-    //       ) : {genre}}
-    //     </Col>       
-    //   </Row>
-
-    // </Container>
 
   )
 }
--- src/MovieList.js
+++ src/MovieList.js
@@ -16,27 +16,17 @@ const MovieList = () => {
 
   if(!movies) return ("Loading...")
 
+
   const movieListStyle ={
     padding: "20px"
   }
-
-  //  const listOfMovies = movies.map(movie => <Movie key={movie.id} movie={movie} />)
-
-  // const list = movies.map(movie => movie)
-  // console.log("moives:", list)
+  // const list = movies.map(movie => movie.genre)
+  // console.log("movies genres:", list)
 
   return(
 
     <Container style={movieListStyle}>
       <Row>
-        <Col><h2>Id</h2>
-        {movies.map((movie)=>(
-          <Movie
-          key={movie.id}
-          id={movie.id}
-          />
-        ))}
-        </Col>
         <Col><h2>Title</h2>
         {movies.map((movie)=>(
           <Movie
@@ -49,7 +39,8 @@ const MovieList = () => {
         {movies.map((movie)=>(
           <Movie
           key={movie.id}
-          genre={movie.genre}/>
+          genre={movie.genre}
+          />
         ))}
         </Col>
       </Row>
